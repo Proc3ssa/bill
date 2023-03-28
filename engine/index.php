be09f978377462784becf43cf5f575033afcc4e1
Processor <Pros3sa@gmail.com>
2023-03-28 11:49:17 +0000

    ui

---
+++ engine/index.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types = 1);
+//include "connection.php";
+
+//vars
+$month = $_POST['month'];
+$amount = $_POST['amount'];
+(int)$numOfApt = $_POST['numOfAp'];
+
+for($i = 1; $i <= $numOfApt; $i++){
+    $apartment.$i = $_POST['apartment1'];
+   
+}
+
+echo "
+    month:$month <br>
+    amount:$amount <br>
+    number of apartments:$numOfApt;
+";
+?>
