6672ba8622071bbd6eab19fa62c445e490d7e071
Processor <Pros3sa@gmail.com>
2023-03-28 11:10:46 +0000

    ui

--- engine/index.php
+++ engine/index.php
@@ -1,21 +1,29 @@
 <?php
 declare(strict_types = 1);
-//include "connection.php";
+include "connection.php";
 
 //vars
 $month = $_POST['month'];
 $amount = $_POST['amount'];
 (int)$numOfApt = $_POST['numOfAp'];
 $apartment = "apartment";
+$aptId = Date('ymdiss');
+
+$insert = 'INSERT into bills values(?,?,?,?)';
+$stmt = $connection -> prepare($insert);
+$stmt -> bind_params("siis", $month, $amount, $numOfApt, $aptsId);
+$stmt -> execute();
+
 for($i = 1; $i <= $numOfApt; $i++){
    
    $apartment.$i = $_POST[$apartment.$i];
-
+   echo $apartment.$i."<br>";
    
    
 }
 
 echo "
+    Apartments ID: $aptId <br>
     month:$month <br>
     amount:$amount <br>
     number of apartments:$numOfApt
